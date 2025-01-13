f4251e15764fb29abee96c6e1542f0af52489305
Limpek <tan4575@hotmail.com>
2025-01-13 21:00:44 +0800

    add services class

---
+++ project/src/services/services.py
@@ -0,0 +1,61 @@
+from threading import Thread,Event,Condition,Lock
+from queue import Queue
+from abc import abstractmethod
+import time
+
+class service(Thread):
+    def __init__(self, group = None, target = None, name = None, args = ..., kwargs = None, *, daemon = None):
+        super().__init__(group, target, name, args, kwargs, daemon=daemon)
+        self._ev_wait_running    : Event     = Event()
+        self._condition     : Condition = Condition()
+        self._name          : str       = name
+        self._running        : bool      = False
+        self._mutex         : Lock      = Lock()
+        self._in_queue      : Queue     = Queue(maxsize=1)
+
+    def start(self):
+        if not self._running:
+            super().start()
+            self.wait_running()
+
+    def set_running(self):
+        self._running = True
+        self._ev_wait_running.set()
+
+    def wait_running(self):
+        if not self._running:
+            self._ev_wait_running.wait()
+
+    def stop(self):
+        self._running = False
+        super().join()
+
+    def run(self):
+        self.work_thread()
+
+
+    def get_queue(self):
+        return self._in_queue
+
+    @staticmethod
+    @abstractmethod
+    def work_thread():
+        raise Exception("no define class!")
+
+
+if __name__ == "__main__":
+
+    class test(service):
+        def __init__(self, group=None, target=None, name=None, args=..., kwargs=None, *, daemon=None):
+            super().__init__(group, target, name, args, kwargs, daemon=daemon)
+
+        def _start(self):
+            self.start()
+
+        def work_thread(self):
+            self.set_running()
+
+    t = test()
+    t._start()
+    time.sleep(5)
+    t.stop()
