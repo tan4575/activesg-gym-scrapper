1dd3eb309cb51e4a13f77379baf712860559913a
Limpek <tan4575@hotmail.com>
2025-01-14 00:27:41 +0800

    remove test.json

--- project/database/Telemetry.sql
+++ project/database/Telemetry.sql
@@ -20,7 +20,7 @@ CREATE TABLE "weather" (
 );
 
 CREATE TABLE "datacamp_courses" (
-  "course_id" SERIAL PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "course_name" VARCHAR(50) UNIQUE NOT NULL,
   "course_instructor" VARCHAR(100) NOT NULL,
   "topic" VARCHAR(20) NOT NULL
