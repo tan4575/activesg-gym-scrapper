6aba3329c992ed58d80ac3de50c4c992c5fbcf48
Limpek <tan4575@hotmail.com>
2025-01-15 00:34:48 +0800

    Use the haversine to identify the closes locations.

--- project/src/controller/weather.py
+++ project/src/controller/weather.py
@@ -1,5 +1,5 @@
 import os, sys
-
+import math
 if __name__ == "__main__":
     import os, sys
     PATH = "/".join(os.path.realpath(__file__).split("/")[0:-2])
@@ -13,38 +13,53 @@ class Weather():
     def __init__(self):
         self.request = sendRequest.sendRequest()
 
+    # haversine formula
+    @staticmethod
+    def haversine( lat1, lon1, lat2, lon2):
+        
+        # distance between latitudes
+        # and longitudes
+        dLat = (lat2 - lat1) * math.pi / 180.0
+        dLon = (lon2 - lon1) * math.pi / 180.0
+    
+        # convert to radians
+        lat1 = (lat1) * math.pi / 180.0
+        lat2 = (lat2) * math.pi / 180.0
+    
+        # apply formulae
+        a = (pow(math.sin(dLat / 2), 2) +
+            pow(math.sin(dLon / 2), 2) *
+                math.cos(lat1) * math.cos(lat2))
+        rad = 6371
+        c = 2 * math.asin(math.sqrt(a))
+        return rad * c
+
+
     def getData(self):
         r1 = self.request.getRainFallData()
         r2 = self.request.getTwoHourForecast()
         retData = []
         location = []
+        bestmatch = ()
         for k in r1.keys():
             r1[k]['area'] = None
-
+            dist = float("inf")
             for k1 in r2.keys():
-                if k1 in r1[k]['location']:
+                coord_dist = self.haversine(r2[k1]['location']["latitude"],
+                                            r2[k1]['location']["longitude"],
+                                            r1[k]['location']["latitude"],
+                                            r1[k]['location']["longitude"])
+                
+                if coord_dist < dist:
+                    dist = coord_dist
                     r1[k]['forecast'] = r2[k1]['forecast']
                     r1[k]['area'] = k1
-                    break
+                    bestmatch = r1[k]['location'],  r1[k]['name']  ,r2[k1]['location'], k1
             location.append(r1[k]['area'])
-
-        for k1 in r2.keys():
-            data = {}
-            if k1 not in location:
-                data['deviceId'] = None
-                data['area']     = k1
-                data['rainfall'] = 0
-                data['forecast'] = r2[k1]['forecast']
-                data['time'] = r2[k1]['timestamp']
-                data['longitude'] = r2[k1]['location']['longitude']
-                data['latitude'] = r2[k1]['location']['latitude']
-                retData.append(data)
         for k in r1:
             data = {}
             data['deviceId'] = k
-            if r1[k].get('area') is not None:
-                data['area'] = r1[k]['area']
-            elif r1[k].get('name') is not None:
+            if r1[k].get('name') is not None:
                 data['area'] = r1[k]['name']
             if r1[k].get('value') is not None:
                 data['rainfall'] = r1[k]['value']
--- project/src/main.py
+++ project/src/main.py
@@ -1,27 +1,6 @@
 from controller import weather, model, gymcapacity
 from database import table
 from logger import logger
-import math
-
-# haversine formula
-def haversine(lat1, lon1, lat2, lon2):
-     
-    # distance between latitudes
-    # and longitudes
-    dLat = (lat2 - lat1) * math.pi / 180.0
-    dLon = (lon2 - lon1) * math.pi / 180.0
- 
-    # convert to radians
-    lat1 = (lat1) * math.pi / 180.0
-    lat2 = (lat2) * math.pi / 180.0
- 
-    # apply formulae
-    a = (pow(math.sin(dLat / 2), 2) +
-         pow(math.sin(dLon / 2), 2) *
-             math.cos(lat1) * math.cos(lat2));
-    rad = 6371
-    c = 2 * math.asin(math.sqrt(a))
-    return rad * c
 
 if __name__ == "__main__":
     logger.logger.info('Started')
@@ -32,7 +11,7 @@ if __name__ == "__main__":
 
     weatherIndex = {}
     for wData in weatherData:
-        logger.logger.info(wData['area'])
+        logger.logger.info(wData)
         weatherIndex[wData['area']] = model.model.insert(table.weather, wData)
 
     for gData in gymData['data']:
@@ -56,7 +35,7 @@ if __name__ == "__main__":
             dist = float("inf")
             bestMatch = ()
             for wData in weatherData:
-                coord_dist = haversine(wData["latitude"],
+                coord_dist = a.haversine(wData["latitude"],
                                  wData["longitude"], 
                                  gymData['data'][gData]['coordinate']['latitude'],
                                  gymData['data'][gData]['coordinate']['longitude'])
@@ -68,40 +47,3 @@ if __name__ == "__main__":
             logger.logger.info("best match : %s, %s", bestMatch[0], bestMatch[1])
         model.model.insert(table.gym_capacity, GYMdata)
     model.model.queryAll(table.gym_capacity)
-
-    # address = b.getAddress()
-    # weatherIndex = {}
-    # for wData in weatherData:
-    #     logger.logger.info(wData['area'])
-    #     weatherIndex[wData['area']] = model.model.insert(table.weather, wData)
-    # for key in gymData['data']:
-    #     GYMdata = {}
-    #     GYMarea = key
-    #     replace= ['ActiveSG','@','Gym','CC','East','West']
-    #     for r in replace:
-    #         GYMarea = GYMarea.replace(r, '')
-    #     GYMarea = GYMarea.strip().lower()
-    #     if gymData['data'][key].lower() == 'closed':
-    #         GYMdata['capacity'] = 0
-    #     else:
-    #         GYMdata['capacity'] = int(gymData['data'][key].replace('%', '').replace('full', '').strip())
-    #     GYMdata['created_at'] = gymData['timestamp']
-    #     GYMdata['location'] = key
-    #     for wData in weatherData:
-    #         if GYMarea in wData['area'].lower() or wData['area'].lower() in GYMarea:
-    #             GYMdata['weather_id'] = weatherIndex[wData['area']]
-    #             break
-    #         else:
-    #             found = False
-    #             for addr in address:
-    #                 if GYMarea in addr:
-    #                     if wData['area'].lower() in addr.lower():
-    #                         GYMdata['weather_id'] = weatherIndex[wData['area']]
-    #                         logger.logger.info("area : %s, %s", wData['area'], GYMarea)
-    #                         found = True
-    #                         break
-    #             if found : break
-    #     model.model.insert(table.gym_capacity, GYMdata)
-
-    # #     # print(model.model.insert(table.weather, data))
-    # model.model.queryAll(table.gym_capacity)
